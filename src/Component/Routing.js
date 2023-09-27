46da552d98d862bbd2e9a5421a2f0f3b6484e7bd
krishnakumar1823 <kumarkrishna180320@gmail.com>
2023-09-28 03:37:21 +0530

    commit

--- src/Component/Routing.js
+++ src/Component/Routing.js
@@ -7,9 +7,9 @@ import { CardDetails } from "./Detail-page"
 export const Routing=()=>{
 
     return(
-        <BrowserRouter>
+        <BrowserRouter  basename={process.env.PUBLIC_URL}>
             <Routes>
-                <Route path="/" element={<Home/>}></Route>
+                <Route path="/" exact element={<Home/>}></Route>
                 <Route path="/cart" element={<Cart/>}></Route>
                 <Route path="/nav-cart" element={<NavCart/>}></Route>
                 <Route path="/cart-details" element={<CardDetails/>}></Route>
